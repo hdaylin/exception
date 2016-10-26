0910df358f79d82d88f4f7eb682262d16882c677
hdaylin <hdaylin@yahoo.com>
2016-10-26 15:36:15 -0400

    Create exception.java

---
+++ exception.java
@@ -0,0 +1,39 @@
+package exception;
+//import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/**
+ *
+ * @author daylinhenry
+ */
+public class Exception {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+ 
+  int a, b, result;
+   
+  try{
+  
+  Scanner input = new Scanner(System.in);
+  System.out.println("Input two integers");
+ 
+  a = input.nextInt();
+  b = input.nextInt();
+ 
+  result = a / b;
+  System.out.println("Result = " + result);
+  
+  
+  }catch(ArithmeticException e){
+    System.out.println("Somethings not right here");  
+  }
+
+  } 
+
+} 
+
+
+     
